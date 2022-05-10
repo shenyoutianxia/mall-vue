10eb968a1b03d947c1d6604c92afe9d088ffe680
shen <shen@shenhao.com>
2022-05-10 14:59:38 +0800

    first commit

---
+++ src/views/common/home.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="mod-home">
+    <div>
+      <h1 class="HeaderClass">欢迎来到谷粒商城后端服务</h1>
+    </div>
+    <div>
+      <h2 class="footerClass"> 作者: 沈浩</h2>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {}
+</script>
+
+<style>
+  .mod-home {
+    line-height: 2;
+  }
+
+  .HeaderClass {
+    font-size: 30px;
+    color: indianred;
+  }
+
+  .mainClass {
+    font-size: 25px;
+    color: green;
+  }
+
+  .footerClass {
+    font-size: 25px;
+    color: blue;
+  }
+</style>
+
